838fb9e1b69e094398dde7a0045ed253ea08bbff
gubriyeeel <officialgabrielsufrir@gmail.com>
2025-01-06 15:10:58 +0800

    chore: add button for profile form

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,22 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="flex flex-col gap-4 w-full max-w-sm">
         <Link
-          className={cn("", buttonVariants({ variant: "default" }))}
+          className={cn("", buttonVariants({ variant: "outline" }))}
           href="/sign-in"
         >
-          Sign in
+          Sign-in Form
         </Link>
         <Link
           className={cn("", buttonVariants({ variant: "outline" }))}
           href="/sign-up"
         >
-          Sign up
+          Sign-up Form
+        </Link>
+        <Link
+          className={cn("", buttonVariants({ variant: "outline" }))}
+          href="/profile"
+        >
+          Profile Form
         </Link>
       </div>
     </main>
